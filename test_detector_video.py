989536e7e04bd5054c6587d7ff7c3ab18cdc6c6a
Gilles Foinet <gfoinet@gmail.com>
2020-12-07 23:31:48 +0100

    update

--- test_detector_video.py
+++ test_detector_video.py
@@ -10,10 +10,24 @@ from PIL import Image, ImageDraw
 from torchvision import transforms as T
 import glob
 import cv2
+from visualize_bbox import visualize
+import numpy as np
 
-cap = cv2.VideoCapture('kangaroos_640_512.avi')
+idx_to_label = {
+    0: 'background',
+    1: 'kangaroo',
+    2: 'other'
+}
+label_to_idx = {v: k for k, v in idx_to_label.items()}
+
+cap = cv2.VideoCapture('kangaroos.mp4')
+fps_in = cap.get(cv2.CAP_PROP_FPS)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+videowriter_out = cv2.VideoWriter('output.avi',fourcc,int(fps_in),(width,height))
 device = torch.device('cuda')
-model = torch.load('test.pt')
+model = torch.load('/home/gilles/ML/kangarooDetection/experiments/20201207_18h12:53s/test.pt')
 model.to(device)
 model.eval()
 
@@ -21,27 +35,15 @@ transformer = T.Compose([
     T.ToTensor(),
     ])
 
-imgList = glob.glob('test/*.jpg')
-
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
     if (ret == True):
-        img = Image.fromarray(frame).convert("RGB")
-
+        img = np.array(Image.fromarray(frame).convert("RGB"))
         imgt = transformer(img).unsqueeze(0)
         pred = model(imgt.to(device))
-        print(pred)
-        
-        # draw
-        base = img.convert("RGBA")
-        txt = Image.new("RGBA", base.size, (255,255,255,0))
-        # get a drawing context
-        d = ImageDraw.Draw(txt)
-        for bbox, score in zip(pred[0]['boxes'].tolist(),pred[0]['scores'].tolist()):
-            if score > 0.:
-                d.rectangle(bbox,fill=None,width = 10)
-        
-        out = Image.alpha_composite(base, txt)
-        if(len(bbox)>=1):
-            out.show()
+        img_out = visualize(img,pred[0]['boxes'].tolist(),pred[0]['labels'].tolist(),idx_to_label,show=False)
+        videowriter_out.write(img_out)
+    else:
+        cap.release()
+videowriter_out.release()
