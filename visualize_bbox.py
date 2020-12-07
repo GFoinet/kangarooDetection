23cc52ebc8db30451cba758b4f55e1124e00c0dc
Gilles Foinet <gfoinet@gmail.com>
2020-12-07 15:50:52 +0100

    add helper for visualization

---
+++ visualize_bbox.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  7 10:27:33 2020
+
+@author: gilles
+"""
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+BOX_COLOR = (255, 0, 0) # Red
+TEXT_COLOR = (255, 255, 255) # White
+
+def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
+    """Visualizes a single bounding box on the image"""
+    x_min, y_min, x_max, y_max = bbox
+    x_min = int(x_min)
+    x_max = int(x_max)
+    y_min = int(y_min)
+    y_max = int(y_max)
+    print((x_min, x_max, y_min, y_max))
+    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
+    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+    cv2.rectangle(img=img, pt1=(x_min, y_min - int(1.3 * text_height)), pt2=(x_min + text_width, y_min), color=BOX_COLOR , thickness=-1)
+    cv2.putText(
+        img,
+        text=class_name,
+        org=(x_min, y_min - int(0.3 * text_height)),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=0.35, 
+        color=TEXT_COLOR, 
+        lineType=cv2.LINE_AA,
+    )
+    return img
+
+def visualize(image, bboxes, category_ids, category_id_to_name):
+    img = image.copy()
+    for bbox, category_id in zip(bboxes, category_ids):
+        class_name = category_id_to_name[category_id]
+        print(class_name)
+        img = visualize_bbox(img, bbox, class_name)
+    plt.figure(figsize=(12, 12))
+    plt.axis('off')
+    plt.imshow(img)
+    plt.show()
